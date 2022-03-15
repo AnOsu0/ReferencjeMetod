ef21f96e0c6771900fa1b462b0658c89b72eef4a
AnOsu0 <as.ania02@gmail.com>
2022-03-15 20:06:08 +0100

    Initial commit

---
+++ src/lesson1/Computer.java
@@ -0,0 +1,52 @@
+package lesson1;
+
+class Computer {
+    private String name;
+    private int cpu;
+    private int temperature;
+
+    public Computer(String name, int cpu, int temperature) {
+        this.name = name;
+        this.cpu = cpu;
+        this.temperature = temperature;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(int cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + cpu + ", " + temperature;
+    }
+
+//     void overclock(){
+//        cpu *= 1.1;
+//        temperature *= 1.1;
+//    }
+
+    static void overclock(Computer computer){
+        computer.cpu *= 1.1;
+        computer.temperature *= 1.1;
+    }
+}
